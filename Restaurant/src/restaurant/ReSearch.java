6ec8551e85fd7096bbd11e46ffb84aae74dea8c9
user <user@129.16.228.190>
2015-11-02 09:58:47 +0100

    Added JFrame and Images for JFrame

---
+++ Restaurant/src/restaurant/ReSearch.java
@@ -0,0 +1,19 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package restaurant;
+
+/**
+ *
+ * @author user
+ */
+public class ReSearch {
+    
+    public static void main(String[] args){
+          SearchFrame formFrame = new SearchFrame();
+        formFrame.setVisible(true);
+        
+    }
+}
