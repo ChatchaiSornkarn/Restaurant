7d7b914f2f3cf6b6b42ee824370e0d7bb24e83a4
asmatkhan <chatchai@dhcp-074087.eduroam.chalmers.se>
2015-11-05 12:32:35 +0100

    Update filter Johan

--- Restaurant/src/restaurant/SQLFilter.java
+++ Restaurant/src/restaurant/SQLFilter.java
@@ -6,20 +6,22 @@
 package restaurant;
 
 import java.sql.*;
+import java.sql.ResultSet;
+import java.sql.Statement;
 import static restaurant.DBConnection.conn;
 
 /**
  *
  * @author chatchai
  */
-public class SQLFilter extends SQLRestaurant{
+public class SQLFilter extends SQLRestaurant {
     
     /**
      * Select all Restaurant name from specific cuisine
      * @param cuisine 
      * @return name of all restaurant in cuisine
      */
-    public static String[] selectFilterCuisine(String cuisine){
+    public static String[] selectFilterCuisineName(String cuisine){
         String[] name = null; 
         
         try{
@@ -54,4 +56,72 @@ public class SQLFilter extends SQLRestaurant{
         return name;
     }  
     
+    public static String[] selectFilterCuisineAddress(String cuisine){
+        String[] address = null; 
+        
+        try{
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("Select Address from Restaurant "
+                    + "INNER JOIN Restaurant_Cuisine_Types USING (Restaurant_ID) "
+                    + "INNER JOIN Cuisine_Types USING (Cuisine_ID)"
+                    + "WHERE Cuisine = \"" + cuisine 
+                    + "\"");
+            
+            int rowcount = 0;
+            
+            if (rs.last()) {
+            rowcount = rs.getRow();
+            rs.beforeFirst();
+            }
+            
+            address = new String[rowcount];
+            int i = 0;
+            
+	    while(rs.next()){
+	        address[i] = rs.getString("Address");
+	        i++;
+            }
+            
+            close(stmt);
+        }
+        catch(Exception e){
+        System.err.println(e.getMessage());
+        }
+        
+        return address;
+    }  
+    public static String[] selectFilterCuisinePhone(String cuisine){
+        String[] phone = null; 
+        
+        try{
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery("Select Telephone from Restaurant "
+                    + "INNER JOIN Restaurant_Cuisine_Types USING (Restaurant_ID) "
+                    + "INNER JOIN Cuisine_Types USING (Cuisine_ID)"
+                    + "WHERE Cuisine = \"" + cuisine 
+                    + "\"");
+            
+            int rowcount = 0;
+            
+            if (rs.last()) {
+            rowcount = rs.getRow();
+            rs.beforeFirst();
+            }
+            
+            phone = new String[rowcount];
+            int i = 0;
+            
+	    while(rs.next()){
+	        phone[i] = rs.getString("Telephone");
+	        i++;
+            }
+            
+            close(stmt);
+        }
+        catch(Exception e){
+        System.err.println(e.getMessage());
+        }
+        
+        return phone;
+    }  
 }
