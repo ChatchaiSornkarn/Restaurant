8622e5938ca0da37c58521dd4f031bd66e095c4e
asmatkhan <chatchai@192.168.0.16>
2016-01-04 20:27:09 +0100

    Delete SQLRegisterStore

--- Restaurant/src/Connection/Server.java
+++ Restaurant/src/Connection/Server.java
@@ -7,7 +7,6 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.ServerSocket;
 import java.net.Socket;
-
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
